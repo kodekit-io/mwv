9b709bbc4bd46da4d83b7e8ede32f26c748350f5
oglab <n.nurjamal@gmail.com>
2016-09-28 19:48:41 +0700

    temporary commit

--- resources/views/mediawave/project-facebook.blade.php
+++ resources/views/mediawave/project-facebook.blade.php
@@ -296,56 +296,6 @@
           'order': [0, 'asc']
        });
 
-       // Handle click on "Select all" control
-       $('#example-select-all').on('click', function(){
-          // Check/uncheck all checkboxes in the table
-          var rows = table.rows({ 'search': 'applied' }).nodes();
-          $('input[type="checkbox"]', rows).prop('checked', this.checked);
-       });
-
-       // Handle click on checkbox to set state of "Select all" control
-       $('#example tbody').on('change', 'input[type="checkbox"]', function(){
-          // If checkbox is not checked
-          if(!this.checked){
-             var el = $('#example-select-all').get(0);
-             // If "Select all" control is checked and has 'indeterminate' property
-             if(el && el.checked && ('indeterminate' in el)){
-                // Set visual state of "Select all" control
-                // as 'indeterminate'
-                el.indeterminate = true;
-             }
-          }
-       });
-
-       $('#frm-example').on('submit', function(e){
-          var form = this;
-
-          // Iterate over all checkboxes in the table
-          table.$('input[type="checkbox"]').each(function(){
-             // If checkbox doesn't exist in DOM
-             if(!$.contains(document, this)){
-                // If checkbox is checked
-                if(this.checked){
-                   // Create a hidden element
-                   $(form).append(
-                      $('<input>')
-                         .attr('type', 'hidden')
-                         .attr('name', this.name)
-                         .val(this.value)
-                   );
-                }
-             }
-          });
-
-          // FOR TESTING ONLY
-
-          // Output form data to a console
-          $('#example-console').text($(form).serialize());
-          console.log("Form submission", $(form).serialize());
-
-          // Prevent actual form submission
-          e.preventDefault();
-       });
     });
         brandChart('brand-equity-container', jQuery.parseJSON('{!! $brandEquity !!}'));
         // shareOfVoice('share-of-voice-container', jQuery.parseJSON('{!! $shareOfVoice !!}'));
